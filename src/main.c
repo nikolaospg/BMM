4d7c89581b54eab36bdd780a278854b1ddde0841
Θοδωρής Τυροβούζης <oogeedev@gmail.com>
2021-09-18 16:59:08 +0300

    Add timing

--- src/main.c
+++ src/main.c
@@ -43,7 +43,29 @@ int main(int argc, char** argv) {
     if (ffile)
         F = CSCfromMM(ffile);
 
+    // timing start
+    struct timespec ts_start;
+    clock_gettime(CLOCK_MONOTONIC, &ts_start);
+
+    // BMM (TODO: configure MPI when needed)
     C = bmm(A, B, F, method, F != NULL);
+
+    // timing end
+    struct timespec ts_end;
+    struct timespec duration;
+    clock_gettime(CLOCK_MONOTONIC, &ts_end);
+    duration.tv_sec = ts_end.tv_sec - ts_start.tv_sec;
+    duration.tv_nsec = ts_end.tv_nsec - ts_start.tv_nsec;
+    while (duration.tv_nsec > 1000000000) {
+        duration.tv_sec++;
+        duration.tv_nsec -= 1000000000;
+    }
+    while (duration.tv_nsec < 0) {
+        duration.tv_sec--;
+        duration.tv_nsec += 1000000000;
+    }
+    double dur_d = duration.tv_sec + duration.tv_nsec/1000000000.0;
+    printf("BMM duration: %lf seconds\n", dur_d);
 
     CSCMatrixfree(A);
     CSCMatrixfree(B);
